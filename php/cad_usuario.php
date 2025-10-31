20d4419d3a39c5e4c591b436fabc9678f8d09050
DESKTOP-LN7SD4G\user <user>
2025-10-31 20:42:43 -0300

    aulinha de php

--- php/cad_usuario.php
+++ php/cad_usuario.php
@@ -4,7 +4,7 @@ $username = $_POST ['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 $confirm = $_POST['confirm'];
-$phonenumber = $_POST['Celular'];
+$phonenumber = $_POST['celular'];
 
 echo "Seu username é ". $username;
 echo "<br>";
@@ -14,14 +14,17 @@ echo "Sua password é ". $password;
 echo "<br>";
 echo "Seu celular é ". $phonenumber;
 
-print_r($_POST);
 //incluir o arquivo de conexão
 include 'conexao.php';
 
 //instrução do sql para inserir dados
-$insert = "INSERT INTO tb_user(null,'$username','$email', '$password', '$phonenumber' )"
+$insert = "INSERT INTO tb_user value (null,'$username','$email', '$password', '$phonenumber' )";
 
+//Executar a instrução sql dentro do banco utilizando a função query()
+$query = $conexao->query($insert);
 
-
+if ($query == true){
+    echo "<script> alert('Usuário cadastrado menó'); window.location.href = '../index.html' <script>";
+}
 
 ?>
